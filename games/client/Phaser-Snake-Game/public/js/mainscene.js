ae9c0a432c6e6409638047ea208f5f280f027de4
PryingKibbles <pryingkibbles@gmail.com>
2024-02-16 10:24:51 -0800

    server

---
+++ games/client/Phaser-Snake-Game/public/js/mainscene.js
@@ -0,0 +1,49 @@
+
+import Snake from './snake.js'
+
+export default class MainScene extends Phaser.Scene{
+    constructor() {
+        super('MainScene')
+        
+        this.player
+    }
+    preload(){
+        this.load.image("snakeHead","/assets/snakeHead.png")
+        this.load.image("snakeHead32","/assets/snakeHead32.png")
+        this.load.image("snakeBody","/assets/snakeBody.png")
+        this.load.image("snakeBody32","/assets/snakeBody32.png")
+        this.load.image("apple","/assets/apple.png")
+        this.load.image("applex32","/assets/applex32.png")
+        this.load.image("portalBlue","/assets/portalBlue.png")
+        this.load.image("wallSing01","/assets/wallSingle01.png")
+        this.load.image("wallSing01x32","/assets/wallSingle01x32.png")
+    }
+    create(){
+        // await this.connect();
+        this.snake = new Snake(this);
+    }
+
+    async connect() {
+		// add connection status text
+		const connectionStatusText = this.add
+			.text(0, 0, "Trying to connect with the server...")
+			.setStyle({ color: "#ff0000", fontSize: "18px" })
+			.setPadding(4);
+
+		const client = new Client(BACKEND_URL);
+		try {
+			this.room = await client.joinOrCreate("snake-server", {
+				playerName: "Testing123",
+			});
+			// connection successful!
+			connectionStatusText.destroy();
+		} catch (e) {
+			// couldn't connect
+			connectionStatusText.text = "Could not connect with the server.";
+		}
+	}
+
+    update(time){
+        this.snake.update(time);
+    }
+}
---
+++ games/client/Phaser-Snake-Game/public/js/singlephase.js
@@ -0,0 +1,20 @@
+import MainScene from "./mainscene.js";
+
+const config = {
+    width: 768,
+    height: 768,
+    type: Phaser.AUTO,
+    parent: 'phaser-game',
+    physics: {
+        default: 'matter',
+        matter: {
+            gravity: {
+                x: 0,
+                y: 0
+            }
+        }
+    },
+    scene: [MainScene]
+};
+
+new Phaser.Game(config);
