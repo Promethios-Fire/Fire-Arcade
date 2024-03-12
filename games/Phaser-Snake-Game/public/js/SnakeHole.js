b5776a1597cad1987c90f3c361a2ca814ef1e000
Holdenian <holdenaatlo@gmail.com>
2024-03-12 09:24:37 -0700

    art update

--- games/Phaser-Snake-Game/public/js/SnakeHole.js
+++ games/Phaser-Snake-Game/public/js/SnakeHole.js
@@ -197,7 +197,7 @@ class GameScene extends Phaser.Scene
         this.add.image(0, GRID*3, 'bg01').setDepth(-1).setOrigin(0,0);
 
         //Boost Meter -- will probably move to a separate UI class - Holden
-        const shape = this.add.rectangle(200, 0, 300, 200, 0xff0000);
+        const shape = this.add.rectangle(200, 0, 300, 200,'#ffffff');
         var boostMeter = this.add.image(GRID * 16,GRID*1,'boostMeter').setDepth(10);
         this.add.image(GRID * 16,GRID*1,'boostMeterFrame').setDepth(10);
         this.mask = shape.createBitmapMask();
