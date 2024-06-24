4de575aed3812e1f5b241810deac111e955f25be
ltj866 <98800297+ltj866@users.noreply.github.com>
2024-06-24 16:29:18 -0700

    stage iceberg version 1

--- games/Phaser-Snake-Game/public/js/SnakeHole.js
+++ games/Phaser-Snake-Game/public/js/SnakeHole.js
@@ -232,7 +232,7 @@ const DREAMWALLSKIP = [0,1,2];
 
 // #region START STAGE
 const START_STAGE = 'Stage-01'; // Warning: Cap sensitive in the code but not in Tiled. Can lead to strang bugs.
-var END_STAGE = 'Stage-12'; // Is var because it is set during debugging UI
+var END_STAGE = 'Stage-06'; // Is var because it is set during debugging UI
 
 
 
