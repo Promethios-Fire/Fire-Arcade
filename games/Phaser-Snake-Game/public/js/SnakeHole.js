ca917a3c04f8dd3d0b21c09d1ed1d4a9f88f076e
ltj866 <98800297+ltj866@users.noreply.github.com>
2024-12-18 01:53:32 -0800

    Split last sections to see better.
    now has 1/2, 1/4, 1/8 have their own sections during over leveled.
    
    Implicitly you can gather 1/16.

--- games/Phaser-Snake-Game/public/js/SnakeHole.js
+++ games/Phaser-Snake-Game/public/js/SnakeHole.js
@@ -916,13 +916,13 @@ class SpaceBoyScene extends Phaser.Scene {
 
             case maxZeds > 17157: // Over leveled.
                 segments = 16; // 131_071   
-                var xOffsets = [1, 4, 7,10, 13, 16, 19, 22, 25, 28, 31, 34, 37, 40, 43, 46];
+                var xOffsets = [1, 3, 6,9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 40, 44, 48];
                 this.add.sprite(startX + 0, barY, 'zedBarSeg1', 1).setDepth(91).setOrigin(0,0);
                 //this.add.sprite(startX + 50, barY, 'zedBarSeg1', 1).setDepth(91).setOrigin(0,0);
                 for (let index = 0; index < segments; index++) {
                     var zedSeg;
-                    if (index === 15) {
-                        zedSeg = this.add.sprite(startX + xOffsets[index], barY, 'zedBarSeg5', 1
+                    if (index === 0) {
+                        zedSeg = this.add.sprite(startX + xOffsets[index], barY, 'zedBarSeg2', 1
                         ).setDepth(91).setOrigin(0,0);
                     } else {
                         zedSeg = this.add.sprite(startX + xOffsets[index], barY, 'zedBarSeg3', 1
